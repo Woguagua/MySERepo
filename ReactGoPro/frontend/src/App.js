ce15bb3a3eb6337ca6f3280083d3114ad9d3afec
Woguagua <3180101985@zju.edu.cn>
2021-03-16 20:05:07 +0800

    Complete Part3

--- ReactGoPro/frontend/src/App.js
+++ ReactGoPro/frontend/src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
+import Header from './components/Header/Header';
+import ChatHistory from './components/ChatHistory/ChatHistory';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    connect();
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            chatHistory: []
+        }
+    }
 
-  send() {
-    console.log("hello");
-    sendMsg("hello");
-  }
+    componentDidMount() {
+        connect((msg) => {
+            console.log("New Message")
+        this.setState(prevState => ({
+            chatHistory: [...this.state.chatHistory, msg]
+    }))
+        console.log(this.state);
+    });
+    }
 
-  render() {
-    return (
-      <div className="App">
+    send() {
+        console.log("hello");
+        sendMsg("hello");
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <Header />
+            <ChatHistory chatHistory={this.state.chatHistory} />
         <button onClick={this.send}>Hit</button>
-      </div>
+        </div>
     );
-  }
+    }
 }
 
 export default App;
